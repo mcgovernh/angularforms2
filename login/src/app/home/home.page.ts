1c0ac76cc686ab70b544d2c1b04b51e4a7cb754f
mcgovernh <mcgovern1969@gmail.com>
2023-03-23 17:36:35 +0000

    angular2-waleed

---
+++ login/src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {Validators,FormControl,FormBuilder,FormGroup} from '@angular/forms'
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  RegisterForm: FormGroup;
+
+  valuefromuser:any;
+  constructor() {}
+  submit(){
+    console.log('value is = ',this.valuefromuser)
+  }
+  ngOnInit(){
+    this.RegisterForm = new FormGroup({
+      name: new FormControl('',[
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8),
+      ])
+
+    })
+  }
+
+}
